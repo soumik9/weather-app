9b428a3487ef77066d61d60454414c0b01fae70a
soumik9 <46122468+soumik9@users.noreply.github.com>
2022-09-10 15:39:38 +0600

    tailwind color, container, font config and nav complete

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <div>
+  <div class="flex flex-col min-h-screen font-Roboto bg-primary">
+    <SiteNav />
     <RouterView />
   </div>
 </template>
 
 <script setup>
   import { RouterView } from 'vue-router';
+  import SiteNav from './components/SiteNav.vue';
 </script>
 
 <style lang="scss" scoped>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": '#00668A',
+        "secondary": '#004E71',
+      }
+    },
+    fontFamily: {
+      Roboto: ["Roboto, sans-serif"],
+    },
+    container: {
+      padding: "2rem",
+      center: true,
+    },
+    screens: {
+      sm: "648px",
+      md: "768px",
+    }
   },
   plugins: [],
 }
